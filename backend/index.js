e0a0092c46f2ae6cbc61c65e207785adaf3cd7b9
Angel Blanco <angelbt91@gmail.com>
2021-02-01 21:19:20 +0100

    Add backend multipart/form-data handling

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,43 @@
 const express = require('express');
-const app = express();
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-app.use(bodyParser.json());
-const port = 3000;
+const multer = require('multer');
+const app = express();
+const upload = multer();
+const port = 3001;
+const Schema = mongoose.Schema;
+const cors = require('cors');
+app.use(cors());
 
+// Create object Schema
+const imageSchema = new Schema({
+    image: Buffer,
+    name: String,
+    size: Number,
+    mimeType: String
+});
+
+// Initialize object Model
+const Image = mongoose.model('Image', imageSchema);
+
+// Handle POST requests
+app.post('/upload', upload.single('image'), (req, res) => {
+    const imgToUpload = new Image({
+        image: req.file.buffer,
+        name: req.file.originalname,
+        size: req.file.size,
+        mimeType: req.file.mimetype
+    });
+
+    imgToUpload.save(function (err, result) {
+        if (err) {
+            res.status(400).send(err);
+        } else {
+            res.status(200).send(result);
+        }
+    });
+});
+
+// Initialize Mongoose
 (async () => {
     await mongoose.connect('mongodb://localhost/', {
         useNewUrlParser: true,
@@ -14,27 +47,7 @@ const port = 3000;
     });
 })();
 
-const Schema = mongoose.Schema;
-
-const imageSchema = new Schema({
-    body: Buffer,
-    'content-type': String
-});
-
-const Image = mongoose.model('Image', imageSchema);
-
-app.post('/', (req, res) => {
-    const newImage = new Image(req.body);
-
-    newImage.save(function (err, result) {
-        if (err) {
-            res.status(400).send(err);
-        } else {
-            res.status(200).send(result);
-        }
-    });
-});
-
+// Start service
 app.listen(port, () => {
     console.log(`Image storing service listening at http://localhost:${port}`)
 });
